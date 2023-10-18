0fba14ae824e42226c21ae73679213dc891c2f94
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-10-18 22:45:36 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+class A {
+    color:string = "red";
+    setColor (color:string){
+        this.color = color
+    }
+}
+const obj1 = new A();
+obj1.setColor("blue")
+console.log("obj1",obj1.color);
+
+const obj2 = new A();
+console.log("obj2",obj2.color);
+
+
+
+// Use static keyword
+// class A {
+//     static color:string = "red";
+//     setColor (color:string){
+//         // this.color = color
+//     }
+//     getColor(){
+//         return A.color
+//     }
+// }
+// const obj1 = new A();
+// obj1.getColor()
+// console.log("obj1",obj1);
+
+// const obj2 = new A();
+// obj2.getColor()
+// console.log("obj2",obj2);
+
